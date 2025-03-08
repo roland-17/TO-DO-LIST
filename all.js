9c354a8d2dc7ff83977561151e55a428c9ee7687
roland-17 <17tiadoueu@gmail.com>
2025-03-08 22:38:45 +0000

    formulaire

---
+++ all.js
@@ -0,0 +1,6 @@
+document.getElementById("conn").addEventListener("click"),()=>{
+document.getElementById("connexion").style.visibility="visible"
+document.getElementById("choix").style.display="none";
+document.getElementById("conn").style.display="none"
+}
+
