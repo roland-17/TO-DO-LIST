449fea1fa8083933f9eac5c2b2f9ac6231b241a2
roland-17 <17tiadoueu@gmail.com>
2025-03-09 09:09:54 +0000

    all

--- all.js
+++ all.js
@@ -1,6 +1,19 @@
-document.getElementById("conn").addEventListener("click"),()=>{
-document.getElementById("connexion").style.visibility="visible"
-document.getElementById("choix").style.display="none";
-document.getElementById("conn").style.display="none"
+let btt1=document.querySelector("#btt1")
+
+btt1.onclick=()=>{
+    document.getElementById("choix").style.display="none";
+    document.getElementById("connexion").style.display="block"; 
+
+
+
 }
+
+let btt2=document.querySelector("#btt2")
+
+btt2.onclick=()=>{
+    document.getElementById("choix").style.display="none";
+    document.getElementById("inscription").style.display="block"
+
+    }
+
 
